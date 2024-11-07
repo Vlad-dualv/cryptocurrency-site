59869a63b33492923a57df472052e528513d14a2
Vlad <vladswork95@gmail.com>
2024-11-07 11:51:32 +0000

    Ready

--- src/main.js
+++ src/main.js
@@ -4,21 +4,29 @@ const bitcoin = document.getElementById('bitcoin');
 const ethereum = document.getElementById('ethereum');
 const dogecoin = document.getElementById('dogecoin');
 
-const API_KEY = 'adtI3iEzrICUmeauFlmvag==C0c4O4zjLM82nYsK';
+const API_KEY = 'CG-P93AkG13D6TDn5Gji5Gd9dVs';
 
 const myAxios = Axios.create({
-  baseURL: 'https://api.api-ninjas.com/v1/cryptoprice/',
+  baseURL:
+    'https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Cdogecoin&vs_currencies=usd',
 });
 
 async function getCurrencies() {
   const params = {
     headers: {
-      'X-Api-Key': API_KEY,
+      accept: 'application/json',
+      'x-cg-demo-api-key': API_KEY,
     },
   };
-  const response = await myAxios.get('', params);
-  const data = response.data;
-  console.log(data);
+  try {
+    const response = await myAxios.get('', params);
+    const data = response.data;
+    bitcoin.textContent = '$' + Number(data.bitcoin.usd).toFixed(2);
+    ethereum.textContent = '$' + Number(data.ethereum.usd).toFixed(2);
+    dogecoin.textContent = '$' + Number(data.dogecoin.usd).toFixed(4);
+  } catch (error) {
+    console.log('Error fetching data:', error);
+  }
 }
 
-//getCurrencies();
+getCurrencies();
